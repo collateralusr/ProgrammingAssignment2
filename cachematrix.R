b98aea9d9b3796a500404bbc9a4da165ca4bf19c
collateralusr <110911796+collateralusr@users.noreply.github.com>
2024-02-04 18:19:26 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+# this here function makes the thing out of the other thing
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(A = matrix()) {
 
-makeCacheMatrix <- function(x = matrix()) {
-
+  A_inv <- NULL
+  
+  set <- function(B) {
+    A <<- B
+    A_inv <<- NULL
+  }
+  get <- function() A
+  set_inv <- function(inv) A_inv <<- inv
+  get_inv <- function() A_inv
+  
+  list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)
+  
 }
 
 
-## Write a short comment describing this function
+# this function right here does the other part.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(thing, ...) {
+
+  A_inv <- thing$get_inv()
+  
+  if (!is.null(A_inv)) {
+    message("gettin', boss")
+    return(A_inv)
+  }
+  
+  A <- thing$get()
+  A_inv <- solve(A)
+  thing$set_inv(A_inv)
+  A_inv
+  
 }
+
+
+# what the above code does:
+
+# > source("cachematrix.R")
+# > M <- matrix(1:4, 2, 2)
+# > M
+#      [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+# > what <- makeCacheMatrix(M)
+# > what$get()
+#      [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+# > what$get_inv()
+# NULL
+# > cacheSolve(what)
+#      [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+# > what$get_inv()
+#      [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+# > solve(M)
+#      [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
