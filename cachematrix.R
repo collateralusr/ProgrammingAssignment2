728e77f8f096ec0c2558ab4b35ad24eca2463e3a
collateralusr <110911796+collateralusr@users.noreply.github.com>
2024-02-04 18:36:29 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,65 +1,76 @@
-# this here function makes the thing out of the other thing
+## Matrix inversion is usually a costly computation and there may be some
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
 
-makeCacheMatrix <- function(A = matrix()) {
+#' Make Cache Matrix
+#'
+#' @name makeCacheMatrix
+#' @usage makeCacheMatrix(x)
+#' @description This function creates a special \code{\link{matrix}} object that
+#'   can cache its inverse.
+#' @param x A matrix (optional)
+#'
+#' @return The list functions to \code{set} and \code{get} the cached
+#'   \code{\link{matrix}} and the its inverse (\code{setInv} and \code{getInv}).
+#' @export makeCacheMatrix
+#'
+#' @examples
+#' makeCacheMatrix()
+#' makeCacheMatrix(matrix(rnorm(9), 3, 3))
+makeCacheMatrix <- function(x = matrix()) {
+    x.inv <- NULL
+    # Set function
+    set <- function(y) {
+        x <<- y
+        x.inv <<- NULL
+    }
 
-  A_inv <- NULL
-  
-  set <- function(B) {
-    A <<- B
-    A_inv <<- NULL
-  }
-  get <- function() A
-  set_inv <- function(inv) A_inv <<- inv
-  get_inv <- function() A_inv
-  
-  list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)
-  
+    # Get function
+    get <- function()
+        x
+
+    # SetInv function
+    setInv <- function(y.inv) {
+        x.inv <<- y.inv
+    }
+
+    # GetInv function
+    getInv <- function()
+        x.inv
+
+    # Return list of set and get functions
+    list(
+        set = set,
+        get = get,
+        setInv = setInv,
+        getInv = getInv
+    )
 }
 
-
-# this function right here does the other part.
-
-cacheSolve <- function(thing, ...) {
-
-  A_inv <- thing$get_inv()
-  
-  if (!is.null(A_inv)) {
-    message("gettin', boss")
-    return(A_inv)
-  }
-  
-  A <- thing$get()
-  A_inv <- solve(A)
-  thing$set_inv(A_inv)
-  A_inv
-  
+#' Cache Solve
+#'
+#' @name cacheSolve
+#' @usage cacheSolve(x, ...)
+#' @description This function computes the inverse of the special
+#'   \code{\link{matrix}} returned by \code{\link{makeCacheMatrix}}. If the
+#'   inverse has already been calculated (and the matrix has not changed), then
+#'   the \code{cacheSolve} retrieves the inverse from the cache.
+#' @param x A matrix
+#' @param ... additional arguments to be passed to \code{\link{solve}}
+#'
+#' @return Returns a matrix that is the inverse of \code{x}
+#' @export cacheSolve
+#'
+#' @examples
+#' x <- makeCacheMatrix(matrix(rnorm(9), 3, 3))
+#' cacheSolve(x) #Not cached
+#' cacheSolve(x) #Cached
+cacheSolve <- function(x, ...) {
+    x.inv <- x$getInv()
+    if (is.null(x.inv)) {
+        x.inv <- solve(x$get(), ...)
+        x$setInv(x.inv)
+    }
+    else
+        message("Getting cached data")
+    x.inv
 }
-
-
-# what the above code does:
-
-# > source("cachematrix.R")
-# > M <- matrix(1:4, 2, 2)
-# > M
-#      [,1] [,2]
-# [1,]    1    3
-# [2,]    2    4
-# > what <- makeCacheMatrix(M)
-# > what$get()
-#      [,1] [,2]
-# [1,]    1    3
-# [2,]    2    4
-# > what$get_inv()
-# NULL
-# > cacheSolve(what)
-#      [,1] [,2]
-# [1,]   -2  1.5
-# [2,]    1 -0.5
-# > what$get_inv()
-#      [,1] [,2]
-# [1,]   -2  1.5
-# [2,]    1 -0.5
-# > solve(M)
-#      [,1] [,2]
-# [1,]   -2  1.5
-# [2,]    1 -0.5
